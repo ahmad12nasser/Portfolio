a8224e99f91947655e56b53a1e642c09d39bcd70
ahmad12nasser <ahmad749nasser123@gmail.com>
2023-12-26 23:46:51 +0200

    Finish the home page and its design completly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router'; // CLI imports router
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import {AppRoutingModule} from "./app-routing.module";
-
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
